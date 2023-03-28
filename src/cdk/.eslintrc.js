439d6746520ef4f20ffafa8e78c6c5264eda7e24
James Fassett <james@zoogeny.com>
2023-03-28 16:45:21 -0700

    Apply common configs to cdk project

--- src/cdk/.eslintrc.js
+++ src/cdk/.eslintrc.js
@@ -1,20 +1,3 @@
 module.exports = {
-  env: {
-    browser: false,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  overrides: [],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {},
+  extends: "../shared/.eslintrc.js",
 };
