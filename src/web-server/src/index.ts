9b1c5eb5a240f9a4639a60a5f4b288671e03bf54
James Fassett <james@zoogeny.com>
2023-04-12 16:16:38 -0700

    src/web-server: POC Google Identity Service (gsi)
    * Super basic html wrapper
    * Client integration to include gsi components and handle auth callback
    * google auth library on server to verify tokens

--- src/web-server/src/index.ts
+++ src/web-server/src/index.ts
@@ -1,13 +1,38 @@
 import express from "express";
+import fs from "fs";
+import { OAuth2Client } from "google-auth-library";
 import path from "path";
 import favicon from "serve-favicon";
 
 const app = express();
 
+app.use(express.json());
+
 app.use(favicon(path.join(__dirname, "../../shared/favicon", "favicon.ico")));
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  const template = fs.readFileSync(path.join(__dirname, "template.html"), {
+    encoding: "utf8",
+  });
+
+  res.send(template.replace("{{ content }}", "Hello, world!"));
+});
+
+app.post("/verify-gis-auth", async (req, res) => {
+  const idToken = req.body.idToken;
+
+  const client = new OAuth2Client(
+    "21613585613-fr67oljpoujbj7vqvhlu9djds142484j.apps.googleusercontent.com"
+  );
+  const ticket = await client.verifyIdToken({
+    idToken,
+    audience:
+      "21613585613-fr67oljpoujbj7vqvhlu9djds142484j.apps.googleusercontent.com",
+  });
+  const payload = ticket.getPayload();
+  console.log("Payload: ", payload);
+
+  res.send({ "result:": "OK" });
 });
 
 app.listen(3000, () => {
